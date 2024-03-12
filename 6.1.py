9a31e3a762b8dfcb0a50be6d052abc8f59e4051b
TonyYep <tonychan@ucsb.edu>
2024-03-12 00:56:51 -0700

    test

---
+++ 6.1.py
@@ -0,0 +1,5 @@
+# 1) Logic Definition of Generalization:
+#(a) Show empirically that the information limit of 2 prediction bits per parameter also holds for nearest neighbors.
+
+
+#(b) Extend your experiments to multi-class classification.
